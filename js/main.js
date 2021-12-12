e231c3ad310f337dbd2315813143170b8ed0362e
HukakHePak <valsidalvonilb@outlook.com>
2021-12-12 12:34:24 +0400

    fix: null field filling, add: some features and shadow, edit: html flexing

--- js/main.js
+++ js/main.js
@@ -8,26 +8,51 @@ const URLS = {
 function addProperty(prop, output) {
     output.textContent = prop;
     output.classList.add('show');   
-    console.log(prop); 
-    setInterval(() => {
-        output.style.background = '#' + Math.floor(Math.random() * 1000);
-    }, 500);
 }
 
+function removeProperty(output) {
+    output.textContent = '';
+    output.classList.remove('show');  
+}
+
+function requestingUrl(url, name, callback) {
+    const requestUrl = `${url}?name=${name}`;
+    fetch(requestUrl)
+    .then(response => response.json())   
+    .then(response => callback(response));       
+}
 
 FLOOR_FORM.form.onsubmit = () => {
-    const name = FLOOR_FORM.name.value;
+    sound();
+    const name = FLOOR_FORM.name.value; 
 
-      if(name) {
-        const floorUrl = `${URLS.floorize}?name=${name}`;
-        fetch(floorUrl).then(resp => resp.json())
-                  .then(resp => addProperty(resp['gender'], FLOOR_FORM.properties[0]))
-        
-        const nationUrl = `${URLS.nationalize}?name=${name}`
-        fetch(nationUrl).then(resp => resp.json())
-                  .then(resp => addProperty(resp['country'][0]['country_id'], FLOOR_FORM.properties[1]))
-                  
-        FLOOR_FORM.form.reset();
-      }   
+    if(name) {
+        requestingUrl(URLS.floorize, name, resp => {                 
+            const floor =  resp['gender'];                
+            const output = FLOOR_FORM.properties[0];           
+            if(gender) addProperty(`${name} is ${floor}`, output);                             
+            else removeProperty(output);           
+        });
+
+        requestingUrl(URLS.nationalize, name, resp => {
+            const output = FLOOR_FORM.properties[1];
+            try {
+                const country = resp['country'][0]['country_id'];
+                addProperty(country, output);
+            }
+            catch {
+                removeProperty(output);
+            } 
+        });
+    }   
+    FLOOR_FORM.form.reset();
     return false;
+}
+
+setInterval(() => {
+    FLOOR_FORM.properties.forEach(prop => prop.style.background = '#' + Math.floor(Math.random() * 1000));
+}, 500);
+
+function sound() {
+    FLOOR_FORM.player.play();
 }
--- js/view.js
+++ js/view.js
@@ -1,7 +1,8 @@
 const FloorForm = document.querySelector('.floorinator');
 
 export const FLOOR_FORM = {
-    form: FloorForm,
+    form: FloorForm.querySelector('.name_in'),
     name: FloorForm.querySelector('[name="name_to_floor"]'),
-    properties: FloorForm.querySelectorAll('.result_text')
+    properties: FloorForm.querySelectorAll('.result_text'),
+    player: FloorForm.querySelector('audio')
 }
